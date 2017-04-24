f42f89bd85e678d88727f3a433ac7b550caa15d9
Johan van Soest <johan@MacBook-Pro-van-Johan.local>
2017-04-24 16:40:44 +0200

    Added run of pentaho and echo test of for workflow

--- clinical-data-workflow/installPentaho.sh
+++ clinical-data-workflow/installPentaho.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install wget unzip
+apt-get install -y wget unzip python python-pip
 
 #download java
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-i586.tar.gz
