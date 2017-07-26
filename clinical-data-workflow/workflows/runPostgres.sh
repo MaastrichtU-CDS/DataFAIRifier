4c908734e49ab6d41034f1abad29fc284eea289b
Johan van Soest <johan.vansoest@maastro.nl>
2017-07-26 22:23:12 +0200

    Added complete execution script

--- clinical-data-workflow/workflows/runPostgres.sh
+++ clinical-data-workflow/workflows/runPostgres.sh
@@ -1,4 +1,14 @@
+docker build -t jvsoest/converter .\
 docker stop cwl_postgres
+docker stop cwl_blazegraph
+
 docker rm cwl_postgres
-#docker run --name cwl_postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /home/johan/Repositories/CAT/DataFAIRifier/clinical-data-workflow/workflows/psql_init/:/docker-entrypoint-initdb.d/ -d postgres
-docker run --name cwl_postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v c:/Users/johan/documents/Repositories/RDF/DataFAIRifier/clinical-data-workflow/workflows/psql_init/:/docker-entrypoint-initdb.d/ -d postgres
+docker rm cwl_blazegraph
+
+docker run -d --name cwl_postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v c:/Users/johan/documents/Repositories/RDF/DataFAIRifier/clinical-data-workflow/workflows/psql_init/:/docker-entrypoint-initdb.d/ postgres
+docker run -d --name cwl_blazegraph -p 9999:9999 jvsoest/blazegraph
+
+docker run --name cwl_converter --link cwl_postgres:postgresdb --link cwl_blazegraph:blazegraph jvsoest/converter
+
+docker stop cwl_converter
+docker rm cwl_converter
