a846135303723aadedbcb1ac01e5fc72d52daa99
Johan van Soest <johan.vansoest@maastro.nl>
2017-07-26 12:44:27 +0200

    Modified local settings #worksOnMyPc

--- clinical-data-workflow/workflows/runPostgres.sh
+++ clinical-data-workflow/workflows/runPostgres.sh
@@ -1,3 +1,4 @@
 docker stop cwl_postgres
 docker rm cwl_postgres
-docker run --name cwl_postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /home/johan/Repositories/CAT/DataFAIRifier/clinical-data-workflow/workflows/psql_init/:/docker-entrypoint-initdb.d/ -d postgres
+#docker run --name cwl_postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /home/johan/Repositories/CAT/DataFAIRifier/clinical-data-workflow/workflows/psql_init/:/docker-entrypoint-initdb.d/ -d postgres
+docker run --name cwl_postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v c:/Users/johan/documents/Repositories/RDF/DataFAIRifier/clinical-data-workflow/workflows/psql_init/:/docker-entrypoint-initdb.d/ -d postgres
