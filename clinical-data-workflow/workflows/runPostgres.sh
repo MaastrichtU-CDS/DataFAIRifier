4801b935217190af5aa423ec51d434008a5f554d
Johan van Soest <johan@MBP-van-Johan-2.fritz.box>
2017-11-22 15:11:52 +0100

    Removed some obvious lines

--- clinical-data-workflow/workflows/runPostgres.sh
+++ clinical-data-workflow/workflows/runPostgres.sh
@@ -5,10 +5,7 @@ docker stop cwl_blazegraph
 docker rm cwl_postgres
 docker rm cwl_blazegraph
 
-docker run -d --name cwl_postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v c:/Users/johan/documents/Repositories/RDF/DataFAIRifier/clinical-data-workflow/workflows/psql_init/:/docker-entrypoint-initdb.d/ postgres
+docker run -d --name cwl_postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /Users/johan/Repositories/DataFAIRifier/clinical-data-workflow/workflows/psql_init/:/docker-entrypoint-initdb.d/ postgres
 docker run -d --name cwl_blazegraph -p 9999:9999 jvsoest/blazegraph
 
-docker run --name cwl_converter --link cwl_postgres:postgresdb --link cwl_blazegraph:blazegraph jvsoest/converter
-
-docker stop cwl_converter
-docker rm cwl_converter
+docker run --rm --link cwl_postgres:postgresdb --link cwl_blazegraph:blazegraph jvsoest/converter
