8cddf3ee4c1a7329f6d9eb57aafba7bc7837cd5b
Johan van Soest <johan.vansoest@maastro.nl>
2018-04-28 17:29:15 +0200

    Made some changes to use R2RML as stand-alone agent

--- clinical-data-workflow/run.py
+++ clinical-data-workflow/run.py
@@ -4,10 +4,10 @@ import urllib
 import os
 import subprocess
 
-# if(os.path.exists("mapping.ttl")):
-#     os.remove("mapping.ttl")
-# if(os.path.exists("out.ttl")):
-#     os.remove("out.ttl")
+if(os.path.exists("mapping.ttl")):
+    os.remove("mapping.ttl")
+if(os.path.exists("out.ttl")):
+    os.remove("out.ttl")
 
 baseUrl = "http://graphdb:7200"
 localGraph = "http://data.local/rdf"
@@ -15,16 +15,16 @@ localGraph = "http://data.local/rdf"
 #####################
 # Get R2RML triples & store in file
 #####################
-#response_r2rml = requests.post(baseUrl + "/repositories/r2rml", 
-#    data="CONSTRUCT { ?s ?p ?o } WHERE { ?s ?p ?o }", 
-#    headers={
-#        "Content-Type": "application/sparql-query",
-#        "Accept": "application/x-turtle"
-#    }
-#)
-#text_file = open("mapping.ttl", "w")
-#text_file.write(response_r2rml.text)
-#text_file.close()
+response_r2rml = requests.post(baseUrl + "/repositories/r2rml", 
+    data="CONSTRUCT { ?s ?p ?o } WHERE { ?s ?p ?o }", 
+    headers={
+        "Content-Type": "application/sparql-query",
+        "Accept": "application/x-turtle"
+    }
+)
+text_file = open("mapping.ttl", "w")
+text_file.write(response_r2rml.text)
+text_file.close()
 
 #####################
 # Run Ontop R2RML
