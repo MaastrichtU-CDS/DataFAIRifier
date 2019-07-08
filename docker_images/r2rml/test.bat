55c81aa92e668aeeac17b5130a8391e636624f2e
Johan van Soest <johan.vansoest@maastro.nl>
2019-07-08 08:21:49 +0200

    Updated test

--- docker_images/r2rml/test.bat
+++ docker_images/r2rml/test.bat
@@ -1,8 +1,8 @@
+rem create an empty output file
 echo. 2>output.ttl
 
+rem Run the docker container in debug mode
 docker run --rm -it ^
-    -v %cd%\run.sh:/run.sh ^
-    -v %cd%\run.py:/run.py ^
     -e DB_JDBC=jdbc:postgresql://172.17.0.1:2345/mydata ^
     -v C:\Users\johan\Documents\Repositories\FAIR\DataFAIRifier\notebooks\mapping.ttl:/mapping.ttl ^
     -v %cd%\output.ttl:/output.ttl ^
